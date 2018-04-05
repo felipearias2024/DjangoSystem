c7814d775330d0b3c771caccce5e77760843f268
MirandaGonzalo <gonzamirandab2000@gmail.com>
2018-04-05 13:30:19 -0300

    Started the views

--- DjangoProyect/app/views.py
+++ DjangoProyect/app/views.py
@@ -4,7 +4,23 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
-
-
 def index(request):
-    return false
+    materias = Curso.objects.all()
+    return render(request,'index.html', {'todos_los_cursos':materias})
+
+def teachersData(request):
+    teachers = Profesor.objects.all()
+    return render(request,'preceptor.html', {'todos_los_profesores':teachers})
+
+def studentsData(request):
+    students = Alumno.objects.all()
+    return render(request, 'alumno.html', {'todos_los_alumnos':students})
+
+def addNota(request):
+    if request.method == 'POST':
+        student_id = request.POST['student_id']
+        student_nota = request.POST['student_nota']
+        alumno = Alumno.objects.get(dni=student_id)
+        nota = Nota(alumno = alumno, nota=student_nota)
+        nota.save()
+        return HttpResponse('{}'.format(alumno.apellido_a), '{}'.format(alumno.nombre_a)
